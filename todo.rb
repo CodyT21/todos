43b8e70dce40651a21c3d4de85b478a216659f7b
Cody Thomas <cody.t.thomas@gmail.com>
2022-12-09 18:06:52 -0700

    Code for assignments 10 - 13

--- todo.rb
+++ todo.rb
@@ -1,6 +1,6 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 configure do
   enable :sessions
@@ -11,8 +11,8 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
 # GET  /lists          -> view all lists
@@ -21,25 +21,35 @@ end
 # GET  /lists/1        -> view a single list
 
 # View all the lists
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
-  erb :lists , layout: :layout
+  erb :lists, layout: :layout
 end
 
 # Render the new list form
-get "/lists/new" do
+get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
 # Create a new list
-post "/lists" do
+def error_message(list_name)
+  if session[:lists].any? { |list| list[:name] == list_name }
+    'That list name already exists.'
+  elsif !(1..100).include? list_name.size
+    'List name must be between 1 and 100 characters.'
+  end
+end
+
+post '/lists' do
   list_name = params[:list_name].strip
-  if list_name.size >= 1 && list_name.size <= 100
-    session[:lists] << { name: list_name, todos: [] }
-    session[:success] = "The list has been created."
-    redirect "/lists"
-  else
-    session[:error] = "List name must be between 1 and 100 characters."
+
+  error = error_message(list_name)
+  if error
+    session[:error] = error
     erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
   end
 end
